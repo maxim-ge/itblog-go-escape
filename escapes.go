1f6daaa34c9f0f198134b1a070f90f79e59a387b
Maxim Geraskin <maxim.ge@gmail.com>
2023-12-05 17:10:26 +0100

    misc

---
+++ escapes.go
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2023-present Maxim Geraskin
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package escapes
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"unsafe"
+)
+
+//go:noinline
+func ReturnValueParamAddress(escapesToHeap Point) *Point {
+	if escapesToHeap.X < 0 {
+		return nil
+	}
+	return &escapesToHeap
+}
+
+func YIfLongest(x, y *string) *string {
+	if len(*y) > len(*x) {
+		return y
+	}
+	s := ""
+	return &s
+}
+
+//go:noinline
+func YIfLongest_noinline(x, y *string) *string {
+	if len(*y) > len(*x) {
+		return y
+	}
+	s := ""
+	return &s
+}
+
+type Point struct {
+	X, Y int
+}
+
+func ReturnPointerParam(leaking *Point) *Point {
+	return leaking
+}
+
+// `p` neither leaks nor escapes
+func ReturnPointerParamField(p *Point) int {
+	return p.X
+}
+
+func ReturnSlice(leaking []byte) []byte {
+	return leaking
+}
+
+// `p` does NOT leak
+func SliceLen(p []byte) int {
+	return len(p)
+}
+
+// ***** Leaking example **********************************************************************************************
+
+// `a` escapes since it leaks on ReturnSlice()
+func CallReturnSlice() {
+	a := make([]byte, 8)
+	fmt.Println(ReturnSlice(a))
+}
+
+// `a` is kept on the stack
+func CallSliceLen(f func([]byte) int) {
+	a := make([]byte, 8)
+	fmt.Println(SliceLen(a))
+}
+
+// ***** Closures ******************************************************************************************************
+
+// `v` and `closure` escape
+func ProvideClosure(closureCaller func(func() int) int) int {
+	var v int
+	closure := func() int {
+		v++
+		return 2
+	}
+	return closureCaller(closure)
+}
+
+type IClosure interface {
+	Do() int
+	ProvideInterfaceMethodAsClosure(closureCaller func(func() int) int) int
+	ProvideFieldAsClosure(func(func() int) int) int
+}
+
+type Closure struct {
+	v       int
+	closure func() int
+}
+
+func (c *Closure) Do() int {
+	c.v++
+	return 2
+}
+
+// c.Do escapes
+func (c *Closure) ProvideInterfaceMethodAsClosure(closureCaller func(func() int) int) int {
+	return closureCaller(c.Do)
+}
+
+// c.closure does NOT escape
+func (c *Closure) ProvideFieldAsClosure(closureCaller func(func() int) int) int {
+	return closureCaller(c.closure)
+}
+
+// ***** binary.Read ******************************************************************************************************
+
+func ReadInt64UsingBinaryRead(r io.Reader) (int64, error) {
+	var v int64
+	err := binary.Read(r, binary.BigEndian, &v)
+	return v, err
+}
+
+func ReadInt64(buf *bytes.Buffer) (res int64, err error) {
+	res, err = int64(binary.BigEndian.Uint64(buf.Next(int(unsafe.Sizeof(res))))), nil
+	return
+}
---
+++ escapes_test.go
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2023-present Maxim Geraskin
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package escapes
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func Benchmark_YIfLongest(b *testing.B) {
+	x := "x"
+	y := "y"
+	for i := 0; i < b.N; i++ {
+		l := YIfLongest(&x, &y)
+		if l == nil {
+			b.Fatal("l is nil")
+		}
+	}
+}
+
+func Benchmark_YIfLongest1_array(b *testing.B) {
+
+	x := [5]string{"a", "ab", "abc", "abcd", "abcde"}
+	y := [5]string{"a", "ab", "abc", "abcd", "abcde"}
+
+	for i := 0; i < b.N; i++ {
+		l := YIfLongest(&x[i%len(x)], &y[i%len(y)])
+		if l == nil {
+			b.Fatal("l is nil")
+		}
+	}
+}
+
+func Benchmark_YIfLongest_noinline(b *testing.B) {
+	x := "x"
+	y := "y"
+	for i := 0; i < b.N; i++ {
+		l := YIfLongest_noinline(&x, &y)
+		if l == nil {
+			b.Fatal("l is nil")
+		}
+	}
+}
+
+func Benchmark_ReturnValueParamAddress(b *testing.B) {
+
+	p := Point{1, 2}
+	for i := 0; i < b.N; i++ {
+		l := ReturnValueParamAddress(p)
+		if l == nil {
+			b.Fail()
+		}
+	}
+}
+
+var closureCaller = func(f func() int) int {
+	return f()
+}
+
+func Benchmark_ProvideClosure(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = ProvideClosure(closureCaller)
+	}
+}
+
+func Benchmark_ProvideInterfaceMethodAsClosure_localCaller(b *testing.B) {
+	closureCaller := func(f func() int) int {
+		return f()
+	}
+
+	c := &Closure{}
+	for i := 0; i < b.N; i++ {
+		_ = c.ProvideInterfaceMethodAsClosure(closureCaller)
+	}
+}
+
+func Benchmark_ProvideInterfaceMethodAsClosure_localCaller_intf(b *testing.B) {
+	closureCaller := func(f func() int) int {
+		return f()
+	}
+
+	var c IClosure = &Closure{}
+	for i := 0; i < b.N; i++ {
+		_ = c.ProvideInterfaceMethodAsClosure(closureCaller)
+	}
+}
+
+func Benchmark_ProvideInterfaceMethodAsClosure_globalCaller(b *testing.B) {
+	c := &Closure{}
+	for i := 0; i < b.N; i++ {
+		_ = c.ProvideInterfaceMethodAsClosure(closureCaller)
+	}
+}
+
+func Benchmark_ProvideFieldAsClosure(b *testing.B) {
+	c := &Closure{}
+	c.closure = c.Do
+	for i := 0; i < b.N; i++ {
+		_ = c.ProvideFieldAsClosure(closureCaller)
+	}
+}
+
+func Benchmark_ReadInt64UsingBinaryRead(b *testing.B) {
+
+	reader := bytes.NewReader([]byte{0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04})
+
+	var err error
+	for i := 0; i < b.N; i++ {
+		_, err = ReadInt64UsingBinaryRead(reader)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_, err = reader.Seek(0, io.SeekStart)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_ReadInt64(b *testing.B) {
+
+	data := []byte{0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04}
+	bytebuffer := bytes.NewBuffer(data)
+
+	var err error
+	for i := 0; i < b.N; i++ {
+		_, err = ReadInt64(bytebuffer)
+		if err != nil {
+			b.Fatal(err)
+		}
+		bytebuffer.Reset()
+		bytebuffer.Write(data)
+	}
+}
